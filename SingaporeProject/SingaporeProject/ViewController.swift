48dc347432370dea3437ca6fb6ae99df637046f0
“saenuruki” <sae.webwork@gmail.com>
2019-09-26 14:35:26 +0900

    create Xcode Project

---
+++ SingaporeProject/SingaporeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingaporeProject
+//
+//  Created by 塗木冴 on 2019/09/26.
+//  Copyright © 2019 塗木冴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

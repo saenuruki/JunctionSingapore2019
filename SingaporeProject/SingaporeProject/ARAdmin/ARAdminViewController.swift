3bd867502c48dec50291929a16b4073cbe4c9bc7
“saenuruki” <sae.webwork@gmail.com>
2019-09-27 10:45:25 +0800

    add MainTabVC

---
+++ SingaporeProject/SingaporeProject/ARAdmin/ARAdminViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ARAdminViewController.swift
+//  SingaporeProject
+//
+//  Created by 塗木冴 on 2019/09/27.
+//  Copyright © 2019 塗木冴. All rights reserved.
+//
+
+import Foundation
--- SingaporeProject/SingaporeProject/AppDelegate.swift
+++ SingaporeProject/SingaporeProject/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let firstViewController = createFirstViewController()
+        
+        self.window?.rootViewController = firstViewController
+        self.window?.makeKeyAndVisible()
+        
+//        FirebaseApp.configure()
+        
         return true
     }
 
@@ -44,3 +52,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate {
+    
+    func createFirstViewController() -> UIViewController {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateInitialViewController() as! MainTabBarController
+        return viewController
+    }
+}
